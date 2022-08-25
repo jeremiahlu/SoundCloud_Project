a746d73a290ef05508cf2fed7cc2f06222c293ca
Jeremiah Lu <jeremiahlu@Jeremiahs-MacBook-Pro.local>
2022-08-25 19:25:16 -0400

    null

--- backend/db/migrations/20220812004631-create-playlist-song.js
+++ backend/db/migrations/20220812004631-create-playlist-song.js
@@ -3,7 +3,6 @@ module.exports = {
   async up(queryInterface, Sequelize) {
     await queryInterface.createTable('PlaylistSongs', {
       id: {
-        allowNull: false,
         autoIncrement: true,
         primaryKey: true,
         type: Sequelize.INTEGER
