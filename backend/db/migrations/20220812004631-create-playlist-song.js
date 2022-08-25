080b4a331cf2fd86382442137262671e1f95aad1
Jeremiah Lu <jeremiahlu@Jeremiahs-MacBook-Pro.local>
2022-08-25 18:43:52 -0400

    mig edit

--- backend/db/migrations/20220812004631-create-playlist-song.js
+++ backend/db/migrations/20220812004631-create-playlist-song.js
@@ -25,7 +25,7 @@ module.exports = {
         defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
       }
     });
-      freezeTableName: true
+      // freezeTableName: true
   },
   async down(queryInterface, Sequelize) {
     await queryInterface.dropTable('playlistSongs');
